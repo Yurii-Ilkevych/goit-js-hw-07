de2d044ad569e8a4830f58770f0016a3ec67c347
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-03-30 17:23:38 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,17 +8,11 @@ const options = {
 
 const galleryEl = document.querySelector(".gallery");
 
-galleryEl.addEventListener("click", selectImage)
-
 const galleryMurkup = createGalleryitems(galleryItems)
 
 galleryEl.insertAdjacentHTML("beforeend", galleryMurkup)
 
 const gallery = new SimpleLightbox('.gallery a', options);
-
-console.log(gallery.options)
-
-gallery.refresh()
 
 function createGalleryitems(image){
 return image.map(({preview, original, description}) => {
@@ -29,30 +23,4 @@ return `<li class="gallery__item">
 </li>`
 
 }).join("")
-}
-
-galleryEl.onclick = function() {
-            return false  }
-
-function selectImage(event) {
-const isOnClass = event.target.classList.contains("gallery__image")
-    if(!isOnClass) {
-        return;
-    }
-     const largeImage = event.target.closest(".gallery__link").href;
-
-     
-
-gallery.on('changed.simplelightbox', function (e) {
-    console.log(this.textContent)
-
-});
-
-
-gallery.on('shown.simplelightbox', function () {
-    console.log(this.firstElementChild)
-
-
-    
-})}
-
+};
